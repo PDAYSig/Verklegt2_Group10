2d396e0661d4a27973531e17567a93120aac592f
PDAYSig <patrikdagur@gmail.com>
2026-05-05 14:01:03 +0000

    completely removed accidental pycache commit

---
+++ users/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+from django.urls import path
+from Art_auctions.urls import urlpatterns
+from django.http import HttpResponse
+# Create your views here.
+
+def index(request):
+    return HttpResponse("Hello, world.")
+
