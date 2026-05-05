7fd8eebf143696b3afacf6b958ca71e064a46667
PDAYSig <patrikdagur@gmail.com>
2026-05-05 17:20:55 +0000

    updated file structure and settings.py

--- users/views.py
+++ users/views.py
@@ -1,10 +1,10 @@
-
+from django.shortcuts import render
 from django.urls import path
 from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    return HttpResponse(f"Hello, world. from {request.path}")
+    return render(request, "users/index.html")
 def user_by_id(request, id):
     return HttpResponse(f"response from {request.path} with id {id}")
 
