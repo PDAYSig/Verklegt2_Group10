f01638743954b5257670278f167e07728ea2b638
DanielDarri <danieldarri2005@gmail.com>
2026-05-06 12:51:30 +0000

    all arts vefsíða kominn

--- users/views.py
+++ users/views.py
@@ -5,6 +5,12 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, "users/index.html")
+
+def all_art(request):
+    images = [f"https://picsum.photos/300?{i}" for i in range(1, 17)]
+    return render(request, "users/all_art.html", {"images": images})
+
+
 def user_by_id(request, id):
     return HttpResponse(f"response from {request.path} with id {id}")
 
