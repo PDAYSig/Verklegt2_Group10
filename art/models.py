c5143e14c428cf2082894107a957552fbf8ffc5d
PDAYSig <patrikdagur@gmail.com>
2026-05-06 11:08:46 +0000

    created migrations for regular user and seller user

--- art/models.py
+++ art/models.py
@@ -2,5 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Art(models.Model):
-    name = models.CharField(max_length=255)
-    
+    def __init__(self):
+        self.id = models.AutoField(primary_key=True)
+        self.title = models.CharField(max_length=200)
+        self.description = models.TextField()
