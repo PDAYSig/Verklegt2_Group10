3c9f20f58bc56d064c28de8fb5a2e411aabf8107
DanielDarri <danieldarri2005@gmail.com>
2026-05-11 10:45:39 +0000

    create_listing komið vel á leið

--- art/models.py
+++ art/models.py
@@ -4,13 +4,29 @@ from django.db import models
 from users.models import Seller
 # Create your models here.
 class art_listing(models.Model):
+    MEDIUM_CHOICES = [
+        ('O', 'Oil'),
+        ('S', 'Sculpture'),
+        ('A', 'Acrylic'),
+        ('W', 'Watercolour'),
+        ('P', 'Photography'),
+    ]
+
+    STYLE_CHOICES = [
+        ('M', 'Modern'),
+        ('A', 'Abstract'),
+        ('R', 'Realism'),
+        ('I', 'Impressionism')
+    ]
+
     id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=255)
     description = models.TextField(max_length=500)
     seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
     artist_name = models.CharField(max_length=255)
-    style = models.CharField(max_length=255)
-    medium = models.CharField(max_length=255)
+    style = models.CharField(max_length=255, choices=STYLE_CHOICES)
+    starting_price = models.IntegerField(default=0)
+    medium = models.CharField(max_length=255, choices=MEDIUM_CHOICES)
     year_created = models.DateField(default=date.today, null=True, blank=True)
     edition = models.CharField(max_length=255, null=True, blank=True)
     provenance = models.TextField(max_length=500, null=True, blank=True)
--- art/views.py
+++ art/views.py
@@ -1,8 +1,10 @@
 from http.client import HTTPResponse
 
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from art.forms.listing_create_form import ListingCreateForm
+from users.models import Seller, Profile
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
@@ -10,10 +12,20 @@ from art.forms.listing_create_form import ListingCreateForm
 def index(request):
     return HTTPResponse(f"Response from {request.path}")
 
+@login_required
 def create_listing(request):
+    form = ListingCreateForm(request.POST or None, request.FILES or None)
+
     if request.method == "POST":
-        print(1)
-    else:
-        return render(request, 'art/create_listing.html', {
-            'form' : ListingCreateForm()
-        })
+        if form.is_valid():
+            listing = form.save(commit=False)
+
+            profile, _ = Profile.objects.get_or_create(user=request.user)
+            seller, _ = Seller.objects.get_or_create(profile=profile)
+
+            listing.seller = seller
+            listing.save()
+
+            return redirect("artwork")
+
+    return render(request, "art/create_listing.html", {"form": form})
