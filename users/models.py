adf65aa895643e3c0082f6c03768ecf759907974
PDAYSig <patrikdagur@gmail.com>
2026-05-12 12:41:51 +0000

    fixed migration for Seller

--- users/models.py
+++ users/models.py
@@ -10,15 +10,19 @@ class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500)
     user_address = models.CharField(max_length=255)
-    profile_image = models.ImageField(upload_to="profiles/", default="profiles/default_profile.png" ,null=True, blank=True)
+    profile_image = models.ImageField(upload_to="profiles/", default="profiles/default_profile.png", null=True, blank=True)
     is_seller = models.BooleanField(default=False)
     def __str__(self):
         return f" {self.user.username} {self.profile_image}"
 
 class Seller(models.Model):
+    TYPE_CHOICES = [
+        ('Individual', 'individual'),
+        ('Gallery', 'gallery'),
+    ]
     id = models.AutoField(primary_key=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    is_gallery = models.BooleanField(default=False)
+    profile = models.OneToOneField(Profile, on_delete=models.CASCADE)
+    type = models.CharField(max_length=255, choices=TYPE_CHOICES)
     rating = models.FloatField(default=0)
     def __str__(self):
-        return f"{self.user.username} {str(self.id)}"
+        return f"{self.profile.user.username} {str(self.id)}"
