51525d4bff094fc77efb7bf71b121d8767528206
PDAYSig <patrikdagur@gmail.com>
2026-05-12 09:15:19 +0000

    updated migrations for artwork to make thumbnail mandatory

--- art/models.py
+++ art/models.py
@@ -19,29 +19,27 @@ class art_listing(models.Model):
         ('Realism', 'Realism'),
         ('Impressionism', 'Impressionism')
     ]
-
     id = models.AutoField(primary_key=True)
+
+    #Artwork information
     title = models.CharField(max_length=255)
     description = models.TextField(max_length=500)
-    seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
     artist_name = models.CharField(max_length=255)
+    year_created = models.DateField(default=date.today, null=True, blank=True)
     style = models.CharField(max_length=255, choices=STYLE_CHOICES)
-    starting_price = models.IntegerField(default=0)
     medium = models.CharField(max_length=255, choices=MEDIUM_CHOICES)
     dimension = models.TextField(max_length=255)
-    year_created = models.DateField(default=date.today, null=True, blank=True)
     edition = models.CharField(max_length=255, null=True, blank=True)
     provenance = models.TextField(max_length=500, null=True, blank=True)
-    date_added = models.DateField(default=date.today)
+
+    #Listing information
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
     starting_price = models.IntegerField(default=0)
-    current_bid = models.IntegerField(default=0)
-    mimimum_bid = models.IntegerField(default=0)
-    # weight_kg = models.FloatField(default=0)
-    thumbnail_image = models.ImageField(
-        upload_to='artworks/',
-        null=True,
-        blank=True
-    )
+    date_added = models.DateField(default=date.today)
+    current_bid = models.IntegerField(default=starting_price)
+    minimum_bid = models.IntegerField(default=0)
+    thumbnail_image = models.ImageField(upload_to='artworks/')
+
     def __str__(self):
         return f"{self.title} by {self.artist_name} \n{self.medium}"
 
