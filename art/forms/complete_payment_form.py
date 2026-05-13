164c7613afa246d6b07e3d1e04c11459c413453e
PDAYSig <patrikdagur@gmail.com>
2026-05-13 13:00:54 +0000

    updated sale migrations and payment form

---
+++ art/forms/complete_payment_form.py
@@ -0,0 +1,16 @@
+from django.forms import ModelForm
+from django_countries.fields import CountryField
+from django import forms
+from art.models import Sale
+class CompletePaymentForm(forms.ModelForm):
+    class Meta:
+        model = Sale
+        exclude = ['id', 'seller', 'buyer', 'listing']
+        widgets = {
+            'buyer_city' : forms.TextInput(attrs={'class': 'form-control'}),
+            'buyer_country' : CountryField(attrs={'class': 'form-control'}),
+            'buyer_street' : forms.TextInput(attrs={'class': 'form-control'}),
+            'buyer_postal_code' : forms.TextInput(attrs={'class': 'form-control'}),
+            'payment_method' : forms.Select(attrs={'class': 'form-control'}),
+            'national_id' : forms.CharField(widget=forms.TextInput(attrs={'class': 'form-control'}),min_length=10, max_length=10),
+        }
