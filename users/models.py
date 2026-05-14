1e027043aa2435272de38fe15006656b4c689e1e
DanielDarri <danieldarri2005@gmail.com>
2026-05-14 15:26:07 +0000

    meira seller info

--- users/models.py
+++ users/models.py
@@ -23,6 +23,8 @@ class Seller(models.Model):
     id = models.AutoField(primary_key=True)
     profile = models.OneToOneField(Profile, on_delete=models.CASCADE)
     type = models.CharField(max_length=255, choices=TYPE_CHOICES)
-    rating = models.FloatField(default=0)
+    street = models.CharField(max_length=255, null=True, blank=True)
+    city = models.CharField(max_length=255, null=True, blank=True)
+    postal_code = models.CharField(max_length=255, null=True, blank=True)
     def __str__(self):
         return f"{self.profile.user.username} {str(self.id)}"
